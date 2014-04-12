8d61b5fd54a1c0c5bc15527786254108c7d38805
Mario Giustiniani <mariogiustiniani@gmail.com>
2014-04-12 15:57:25 +0200

    add docs

--- Routing/Router.php
+++ Routing/Router.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Manticora\RestExtraBundle\Routing;
 use Symfony\Bundle\FrameworkBundle\Routing\Router as BaseRouter;
 use Symfony\Component\Config\ConfigCache;
