dfdfc02b18e4e56c5b40f6f52c24317566766b96
Mario Giustiniani <mariogiustiniani@gmail.com>
2014-05-04 16:54:26 +0200

    remove useless test

--- Tests/ApiVersioning/RouterApiTest.php
+++ Tests/ApiVersioning/RouterApiTest.php
@@ -18,10 +18,6 @@ class RouterApiTest extends \PHPUnit_Framework_TestCase
 {
     public function testCondition()
     {
-        $route = new Route('/');
-        $this->assertEquals(null, $route->getCondition());
-        $route->setCondition('context.getMethod() == "GET"');
-        $this->assertEquals('context.getMethod() == "GET"', $route->getCondition());
 
         $coll = new RouteCollection();
         $route = new Route('/foo');
